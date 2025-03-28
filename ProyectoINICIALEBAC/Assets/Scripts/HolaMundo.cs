6d56a6b813b34b98cc10edb59a325c79d726dc32
LuxDeluxe21 <lucianomoya1995@gmail.com>
2025-03-28 00:21:29 -0300

    Clase módulo 5
    Paso a paso del módulo 5 de Gameloop

--- ProyectoINICIALEBAC/Assets/Scripts/HolaMundo.cs
+++ ProyectoINICIALEBAC/Assets/Scripts/HolaMundo.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class HolaMundo : MonoBehaviour
 {
+    private int x;
+
     // Start is called before the first frame update
     void Start()
     {
+        x = 0;
         print("algo paso");
-        Debug.Log("Algo Paso");
+
         Debug.LogWarning("Algo Salio Medianamente Mal");
         Debug.LogError("Algo Salio Muy mal");
     }
@@ -16,6 +19,27 @@ public class HolaMundo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //X = X + 1;
+        //Debug.Log(X);
+
+        Debug.Log("Hola desde Update");
+    }
+    private void FixedUpdate()
+    {
+        Debug.LogWarning("Hola desde Fixed Update cada 50 frames");
+    }
+    private void LateUpdate()
+    {
+        Debug.Log("Hola desde Late Update");
+    }
+
+    private void OnEnable()
+    {
+        Debug.LogWarning("El Objeto ha sido Habilitado");
+    }
+
+    private void OnDisable()
+    {
+        Debug.LogWarning("El objeto ha sido inhabilitado");
     }
 }
